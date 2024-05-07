39a52b2dfd5fa80521481747c4585e2b086390e2
kuan <ulamaca.lee@gmail.com>
2024-05-07 22:26:42 +0800

    feat: (1) added comprehensive cv scores, (2) added comprehensive test score, (3) replace SVM to RF by default (done 1min in at CPU/laptop)
    finding:
    1) svm would produce weirdly behaving probability prediction
    2) replace it with RF, can mitigate the issue
    3) and with RF predictor, GA is able to learn to improve!

--- fit_map4_svm_hemo.py
+++ fit_map4_svm_hemo.py
@@ -7,13 +7,16 @@ import os
 import numpy as np
 import pandas as pd
 from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn import preprocessing
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
+from sklearn.metrics import matthews_corrcoef, precision_score, recall_score, f1_score, accuracy_score
 import joblib
 from config import MODEL_PATH
+import json
 
 # data prep
 def train_clf(clf, X, Y, x, y):
@@ -23,15 +26,32 @@ def train_clf(clf, X, Y, x, y):
 
     roc_training = roc_auc_score(Y, predicted_training[:,1])
     roc_test = roc_auc_score(y, predicted_test[:,1])
+    
+    y_test = clf.predict(x)
+    acc_test = accuracy_score(y, y_test)
+    precision_test = precision_score(y, y_test)
+    recall_test = recall_score(y, y_test)
+    f1_test = f1_score(y, y_test)
+    mcc_test = matthews_corrcoef(y, y_test)
 
-    print("ROC AUC Train: {:.3f}".format(roc_training))
-    print("ROC AUC Test: {:.3f}".format(roc_test))
+    result = {
+        'roc-auc_training': roc_training,
+        'roc-auc_test': roc_test,
+        'acc_test': acc_test,
+        'precision_test': precision_test,
+        'recall_test': recall_test,
+        'f1_test': f1_test,
+        'mcc_test': mcc_test
+    }
+    print(json.dumps(result, indent=4))
+    
     return clf
 
 NORM = True
 norm_types = ['std', 'minmax']
 NORM_TYPE = norm_types[1]
 GRID_SEARCH = False
+
 if __name__ == "__main__":
     df = pd.read_csv('data/dbaasp/fine_tune_hemolysis.csv')
     df = df.query('isNotHemolytic >= 0') # leaving only labeled data
@@ -60,8 +80,7 @@ if __name__ == "__main__":
             raise ValueError
         X = scaler.fit_transform(X)
         x = scaler.transform(x)
-        
-
+            
     # prep X,Y,x,y
     # training        
     if GRID_SEARCH:
@@ -77,16 +96,21 @@ if __name__ == "__main__":
         best_config_svm = SVC(**best_parmas)
         SVM = train_clf(best_config_svm, X, Y, x, y)
     else:
-        # cv
-        clf = SVC(probability=True, gamma = 1.0, C=0.1)
+        # cv        
+        clf = RandomForestClassifier(n_estimators=500, max_depth=10)
         # cross-validation
-        # shuffle = KFold(n_splits=5, shuffle=True, random_state=42)
-        # cv_scores = cross_val_score(clf, X, Y, cv=shuffle, scoring='roc_auc')
-        # print('cross-validation auc_roc:', cv_scores)
-        SVM = train_clf(clf, X, Y, x, y)
-        joblib.dump(SVM, os.path.join(MODEL_PATH, 'hemo_svm_minmax_norm_map4.joblib'))
-        joblib.dump(scaler, os.path.join(MODEL_PATH, 'hemo_scaler_minmax_norm_map4.joblib'))
+        shuffle = KFold(n_splits=5, shuffle=True, random_state=42)
+        cv_score_dict = {
+            'roc_auc': cross_val_score(clf, X, Y, cv=shuffle, scoring='roc_auc'),
+            'f1': cross_val_score(clf, X, Y, cv=shuffle, scoring='f1'),
+            'accuracy': cross_val_score(clf, X, Y, cv=shuffle, scoring='accuracy'),
+            'precision': cross_val_score(clf, X, Y, cv=shuffle, scoring='precision'),
+            'recall': cross_val_score(clf, X, Y, cv=shuffle, scoring='recall')
+        }
+        df_scores = pd.DataFrame(cv_score_dict)
+                                         
+        clf = train_clf(clf, X, Y, x, y)        
+        joblib.dump(clf, os.path.join(MODEL_PATH, 'hemo_rf_minmax_norm_map4.joblib'))        
 
-    
-    
+        
     breakpoint()
