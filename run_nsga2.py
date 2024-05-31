2dc22bc05d458e4c67490397f8c41f14c7c446bf
kuan <ulamaca.lee@gmail.com>
2024-05-31 21:52:12 +0800

    feat: added result output (df_trace) and evolution visualization function (plot_trace_of_ga)

--- run_nsga2.py
+++ run_nsga2.py
@@ -18,6 +18,25 @@ class KMerTransformer(BaseEstimator, TransformerMixin):
         #     return []
         return [sequence[i:i+self.k] for i in range(len(sequence) - self.k + 1)]
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime
+
+def plot_trace_of_ga(df_trace, score_1, score_2, n_fronts=5):
+    plt.figure(figsize=(10, 6))
+    df_trace = df_trace.query(f'rank <= {n_fronts}')
+    scatter = sns.scatterplot(data=df_trace, x=score_1, y=score_2, hue='generation', palette='viridis')
+    plt.xlabel(f'score: {score_1}')
+    plt.ylabel(f'score: {score_2}')
+    plt.title('NSGA-II Evolution Trace')
+    plt.legend()
+    today_str = datetime.date.today().strftime('%y%m%d')    
+    gen = df_trace['generation'].max()
+    plt.savefig(f'assets/analysis/evo_traces/{today_str}_gen={gen}_rank<={n_fronts}.png')
+
+    # Show plot
+    plt.show()
+
 if __name__ == '__main__':
     hemo_scorer = joblib.load(HEMO_SCORE_PIPELINE_PATH)
     mic_scorer = joblib.load(MIC_SCORE_PIPELNE_PATH)
@@ -25,5 +44,8 @@ if __name__ == '__main__':
         'rf_hemo_clf': hemo_scorer,
         'rf_mic_clf': mic_scorer
     }
-    ga = NSGA2(scorers=scorer_dict)
-    ga.run()
+    ga = NSGA2(scorers=scorer_dict,
+               num_generations=15)
+    df_trace = ga.run()
+    plot_trace_of_ga(df_trace, 'rf_hemo_clf', 'rf_mic_clf')
+    
